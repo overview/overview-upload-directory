a06dd4efb072e501131bdf44ec9ceab3a8fd8a21
jstray <jonathanstray@gmail.com>
2017-03-14 17:55:34 -0400

    Upload a whole dir

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # overview-upload-directory
 A script to upload / sync a directory of document files to an Overview server
+
+# usage
+`python upload.py -t <API_TOKEN> -s <SERVER_URL> [-n] file`
+
+* `API_TOKEN` is the access token for a particular document set, which you get like this
+
+1. Browse to your document set. The URL will look like `https://www.overviewdocs.com/documentsets/123456`
+2. We'll refer to the `123456` part of the URl above as `$DOCUMENT_SET_ID`.
+3. Browse to `https://www.overviewdocs.com/documentsets/$DOCUMENT_SET_ID/api-tokens` and click "Generate token".
+
+* `SERVER_NAME` is the base URL for the server, which defaults to http://localhost:9000 for use with overview-local
+
+* `-n` means don't skip already uploaded files, if specified
+
+* `file` is the name of a file, or a directory. 
+
+Note that the document tiles in Overview will have any preceding path stripped off. So `upload some/path/to/directory` will produce titles of the form `directory/file1`, `directory/subdir/file2`, etc. If you want to preserve `some/path/to`, do `upload some`
+
+Requires python3
+
--- upload.py
+++ upload.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# Upload a file, or directory, to an Overview server. 
+# From https://github.com/overview/overview-upload-directory
+
 import requests
 import sys
 import os
@@ -28,18 +32,17 @@ def upload_single_file(server_url, api_token, filename, skip_existing=False):
 	bytes = open(filename, 'rb').read()
 
 	if skip_existing and file_already_on_server(server_url, api_token, bytes):
-		print(filename + " already on server, skipping")
+		print("Skipping " + filename + ", already on server")
 		return
 	else:
 		print("Uploading " + filename + "...")
 
 	url = server_url + '/api/v1/files/' + str(uuid.uuid4())
-
 	headers = { 'content-disposition' : content_disposition(filename),
 				'content-length' : str(os.path.getsize(filename)) }
-
 	r = requests.post(url, auth=(api_token,'x-auth-token'), headers=headers, data=bytes)
 	r.raise_for_status()
+
 	files_uploaded += 1
 
 
@@ -52,6 +55,19 @@ def finish_upload(server_url, api_token):
 	r = requests.post(url, auth=(api_token,'x-auth-token'), headers=headers, data=json.dumps(data))
 	r.raise_for_status()
 
+def upload_directory(server_url, api_token, filename, skip_existing=False):
+	for subdir, dirs, files in os.walk(filename):
+	    for file in files:
+
+	    	if file[0]=='.':
+	    		continue		# don't upload normally hidden files (I'm looking at you, .DS_store)
+
+	    	fullname = os.path.join(subdir, file)
+	    	try:
+	    		upload_single_file(server_url, api_token, fullname, skip_existing=skip_existing)
+	    	except:
+	    		print("Error, skipping " + fullname)
+
 
 # ---- Main ----
 
@@ -59,12 +75,34 @@ parser = argparse.ArgumentParser(description='Upload a file or directory to an O
 parser.add_argument('file', help='file or directory to upload')
 parser.add_argument('-t', '--token', help='API token corresponding to document set', required=True)
 parser.add_argument('-s', '--server', help='url of Overview server, defaults to http://localhost:9000', default='http://localhost:9000')
-parser.add_argument('-n', '--noskip', help='Don\'t skip documents already in this document set', action="store_true")
+parser.add_argument('-n', '--noskip', help='Don\'t skip files already on server', action="store_true")
 args = parser.parse_args()
 
-upload_single_file(args.server, args.token, args.file, skip_existing=not args.noskip)
-if files_uploaded > 0:
-	finish_upload(args.server, args.token,)
+skip_existing = not args.noskip
+
+filename = args.file.rstrip('/')  # removing trailing slash important for consistent directory handling
+
+if not os.path.exists(filename):
+	print("Cannot find file or directory " + filename)
+else:
+	ispath = os.path.isdir(filename)
+
+	# do this all from directory containing specified file, to get consistent file titles 
+	# if user specified a file, the filename (no path) is a title
+	# if user specified a directory, that directory name (no path) is the base of the title
+	basename = os.path.basename(filename)	
+	dirname = os.path.dirname(filename)
+	if dirname!='':
+		os.chdir(dirname)
+
+	if ispath:
+		upload_directory(args.server, args.token, basename, skip_existing=skip_existing)
+	else:
+		upload_single_file(args.server, args.token, basename, skip_existing=skip_existing)
+
+	# Send a finish only if we actually uploaded something (may have skipped all)
+	if files_uploaded > 0:
+		finish_upload(args.server, args.token,)
 
 
 
