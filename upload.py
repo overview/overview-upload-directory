50da00a034971db672015e213cf09cadc71968e7
Adam Hooper <adam@adamhooper.com>
2017-05-31 18:10:21 -0400

    Pass CSRF check

--- upload.py
+++ upload.py
@@ -53,8 +53,11 @@ def upload_single_file(server_url, api_token, filename, skip_existing=False):
 		print("Uploading " + filename + "...")
 
 	url = server_url + '/api/v1/files/' + str(uuid.uuid4())
-	headers = { 'content-disposition' : rfc6266.build_header(filename),
-				'content-length' : str(os.path.getsize(filename)) }
+	headers = {
+            'Content-Disposition' : rfc6266.build_header(filename),
+            'Content-Length' : str(os.path.getsize(filename)),
+            'X-Requested-With' : 'overview-upload-directory'
+    }
 	r = requests.post(url, auth=(api_token,'x-auth-token'), headers=headers, data=bytes)
 	r.raise_for_status()
 
